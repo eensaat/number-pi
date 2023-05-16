43f0ef9b84a36636456fcc150be0a17c28072bfc
eensaat <118106185+eensaat@users.noreply.github.com>
2023-05-16 19:22:43 +0330

    Add files via upload

---
+++ Ali.Safarnezhad.Assessment.3.py
@@ -0,0 +1,5 @@
+a = float(input("please inter pi number : "))
+b = float(input("please inter diameter : "))
+c = float(3.141*b)
+d = float(3.141*(b/2)**2)
+print(f'{format(d,'.3f')}{format(c,'.3f')}')
